8241a2888c75049e3efbc3834d56b0180e550b0a
Kenneth Huang <kenneth.noodle@gmail.com>
2017-07-21 22:45:26 -0700

    add void damage, make lightning bolt damage closer to original

--- src/main/java/profhugo/nodami/handlers/EntityHandler.java
+++ src/main/java/profhugo/nodami/handlers/EntityHandler.java
@@ -14,9 +14,13 @@ public class EntityHandler {
 		DamageSource source = event.getSource();
 		//May have more DoTs missing in this list
 		if (source.equals(DamageSource.IN_FIRE) || source.equals(DamageSource.LAVA) || source.equals(DamageSource.CACTUS)
-				|| source.equals(DamageSource.LIGHTNING_BOLT) || source.equals(DamageSource.IN_WALL)
-				|| source.equals(DamageSource.HOT_FLOOR)) {
+				|| source.equals(DamageSource.IN_WALL)|| source.equals(DamageSource.HOT_FLOOR)
+				|| source.equals(DamageSource.OUT_OF_WORLD)) {
 			event.setAmount(event.getAmount() / 10);
+		}
+		// A lightning bolt seems to persist for 5 ticks, this should make damage equal
+		else if (source.equals(DamageSource.LIGHTNING_BOLT)) {
+			event.setAmount(event.getAmount() / 5);
 		}
 		entity.hurtResistantTime = 0;
 		entity.hurtTime = 1;
