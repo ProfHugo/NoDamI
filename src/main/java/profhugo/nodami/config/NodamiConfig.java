4e75eebb3922587c87a362d2b818828e2dce4975
ProfHugo <hugothepvpmaster0722@gmail.com>
2018-12-26 00:13:46 -0800

    added thaumic slime to the exclusion list

--- src/main/java/profhugo/nodami/config/NodamiConfig.java
+++ src/main/java/profhugo/nodami/config/NodamiConfig.java
@@ -27,7 +27,7 @@ public class NodamiConfig {
 	private static void syncConfig() {
 		attackCancelThreshold = config.getFloat("attackCancelThreshold", "Thresholds", 0.1f, -0.1f, 1, "How weak a player's attack can be before it gets nullified, from 0 (0%, cancels multiple attacks on the same tick) to 1 (100%, players cannot attack), or -0.1 (disables this feature)");
 		knockbackCancelThreshold = config.getFloat("knockbackCancelThreshold", "Thresholds", 0.75f, -0.1f, 1, "How weak a player's attack can be before the knockback gets nullified, from 0 (0%, cancels multiple attacks on the same tick) to 1 (100%, no knockback), or -0.1 (disables this feature)");
-		attackExcludedEntities = config.getStringList("attackExcludedEntities", "Exclusions", new String[] {"minecraft:slime", "tconstruct:blueslime"}, "List of entities that need to give iFrames on attacking");
+		attackExcludedEntities = config.getStringList("attackExcludedEntities", "Exclusions", new String[] {"minecraft:slime", "tconstruct:blueslime", "thaumcraft:thaumslime"}, "List of entities that need to give iFrames on attacking");
 		dmgReceiveExcludedEntities = config.getStringList("damageReceiveExcludedEntities", "Exclusions", new String[0], "List of entities that need to receive iFrames on receiving attacks or relies on iFrames");
 	
 		if (config.hasChanged()) {
